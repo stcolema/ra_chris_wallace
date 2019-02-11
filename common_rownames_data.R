ab67cf7d94eab4db6b3e48eb588126158f9aa65a
Stephen Coleman <stcolema@tcd.ie>
2019-02-11 16:14:34 +0000

    Now save row_names appropriately for MDI tool (i.e. using a separate column)

--- common_rownames_data.R
+++ common_rownames_data.R
@@ -21,12 +21,9 @@ prep_data <- function(dt) {
 # files_present <- list.files(path = args$dir)
 # file_name <- grep(args$extension, files_present, value = TRUE) %>%
 #   paste0(args$dir, "/", .)
+setwd("~/Desktop/My_end/")
 files_present <- list.files(path = "~/Desktop/My_end/")
 file_name <- grep(".csv", files_present, value = TRUE)
-num_datasets <- length(file_name)
-data_dt <- data.table(Data = data.table())
-
-
 
 data_lst <- list()
 setwd("~/Desktop/My_end")
@@ -52,34 +49,34 @@ for(f in file_name){
 # Move the genes present to a list
 genes_present %<>% unname() %>% unlist()
 
-# Fro each dataset filter by genes present in all and write to file
+# For each dataset filter by genes present in all and write to file
 for(f in names(data_lst)){
   data_lst[[f]] %<>%
     filter(V1 %in% genes_present)
-  
+
   file_write <- strsplit(f, "_?_(.*?)_?")[[1]][2] %>% paste0(., ".csv")
   
-  fwrite(data_lst[[f]], file = file_write, row.names = T)
+  fwrite(data_lst[[f]], file = file_write, row.names = F)
 }
 
-lapply(data_lst, nrow)
-
-dt_1 <- fread("~/Desktop/My_end/transposed_TR_GE_Corrected4_Covars.csv", header = T)
-dt_2 <- fread("~/Desktop/My_end/transposed_CD4_GE_Corrected4_Covars.csv", header = T)
-
-# Find common genes present in both datasets
-genes_present <- semi_join(
-  select(dt_1, V1),
-  select(dt_2, V1),
-  by = "V1"
-) %>%
-  unname() %>%
-  unlist()
-
-# Filter out any genes not present in all datasets
-dt_1_small <- dt_1 %>% filter(V1 %in% genes_present) %>% prep_data()
-dt_2_small <- dt_2 %>% filter(V1 %in% genes_present) %>% prep_data()
-
-# Write to file
-fwrite(dt_1_small, file = transposed_TR_GE.csv, row.names = T)
-# fwrite(dt_out, file = file_to_write, row.names = T)
+# lapply(data_lst, nrow)
+# 
+# dt_1 <- fread("~/Desktop/My_end/transposed_TR_GE_Corrected4_Covars.csv", header = T)
+# dt_2 <- fread("~/Desktop/My_end/transposed_CD4_GE_Corrected4_Covars.csv", header = T)
+# 
+# # Find common genes present in both datasets
+# genes_present <- semi_join(
+#   select(dt_1, V1),
+#   select(dt_2, V1),
+#   by = "V1"
+# ) %>%
+#   unname() %>%
+#   unlist()
+# 
+# # Filter out any genes not present in all datasets
+# dt_1_small <- dt_1 %>% filter(V1 %in% genes_present) %>% prep_data()
+# dt_2_small <- dt_2 %>% filter(V1 %in% genes_present) %>% prep_data()
+# 
+# # Write to file
+# fwrite(dt_1_small, file = transposed_TR_GE.csv, row.names = T)
+# # fwrite(dt_out, file = file_to_write, row.names = T)
