d3835a49589c24a19a2e6a7ac6d104adb7544063
Stephen Coleman <stcolema@tcd.ie>
2019-03-04 10:49:17 +0000

    MDI analysis files

---
+++ Analysis/analysis_na_datasets.R
@@ -0,0 +1,514 @@
+#!/usr/env/bin Rscript
+
+# /home/MINTS/sdc56/Desktop/MDI/mdipp-1.0.1/mdipp
+# N CD14.csv
+# N CD15.csv
+# N CD19.csv
+# N CD4.csv
+# N CD8.csv
+# N IL.csv
+# N PLA.csv
+# RE.csv
+# TR.csv
+# -n 10000
+# -t 25
+# -s 1
+# > output_full_sets.csv
+
+# === Libraries ================================================================
+
+source("~/Desktop/MDI/mdipp-1.0.1/scripts/analysis.R")
+library(magrittr)
+library(dplyr)
+library(data.table)
+library(rlist)
+library(pheatmap) # install.packages("pheatmap", dep = T)
+library(RColorBrewer)
+# output <- loadDataGauss("~/Desktop/First attempt/output_1.csv")
+
+# === Setup ====================================================================
+
+setwd("~/Desktop/MDI/Data/Full_sets")
+
+# Read in the probes present - this is a matrix of bools with the first column
+# as the probe IDs and the remaining columns corrresponding to the cell types
+# with TRUE indicating the probe is present in this cell type (i.e. not added
+# manually with an imputed value) and FALSE indicates we added it in.
+probes_present_dt <- fread("~/Desktop/MDI/Data/probes_present_per_dataset.csv")
+
+# Read in the file relating the probe IDs to  the related gene
+probe_key <- fread("~/Desktop/MDI/Data/probe_key.csv")
+
+# Read in the MDI output file
+mdi_output_file <- "output_full_sets.csv"
+
+# Convert this into useable output using functions provided by Sam Mason
+mcmc_output <- readMdiMcmcOutput(mdi_output_file)
+
+clust_occ <- getClustersOccupied(mcmc_output)
+head(clust_occ)
+
+mdi_alloc <- getMdiAllocations(mcmc_output, 1)
+head(mdi_alloc[1:6, 1:6])
+
+psm_1 <- genPosteriourSimilarityMatrix(mdi_alloc)
+
+# clustering_1 <- hclust(psm_1)
+
+head(psm_1[1:6, 1:6])
+
+x <- c1[1:5]
+x2 <- c1[6:10]
+x3 <- c1[11:15]
+
+clux <- list(x, x2, x3)
+6900475 %in% names(unlist(clux))
+
+small_psm <- psm_1[1:10, 1:10]
+
+clusters <- list()
+probes_included <- c()
+n_clust <- 0
+for(i in 1:nrow(psm_1)){
+  probe_id <- row.names(psm_1)[i]
+  if(probe_id %in% probes_included){
+    next
+  }
+  n_clust = n_clust + 1
+  clusters[[n_clust]] <- psm_1[i, psm_1[i,] <= 0.20]
+  probes_included <- c(probes_included, names(clusters[[n_clust]]))
+  if(all(row.names(psm_1) %in% probes_included)){
+    break
+  }
+}
+
+new_clusts <- list()
+score <- matrix(0, nrow = n_clust, ncol = n_clust)
+for(i in 1:n_clust){
+  curr_clust <- clusters[[i]]
+  members <- names(curr_clust)
+  threshold <- 0.5
+  
+  
+  
+  for(j in 1:n_clust){
+    if(j == i) {
+      next
+    }
+    comp_members <- names(clusters[[j]])
+    score[i, j] <- sum(members %in% comp_members) / length(curr_clust)
+    
+  }
+  # if(max(score[i, ] > threshold)){
+  #   merge_clust <- which.max(score[i, ])
+  #   new_cluster <- unique(c(members, names(clusters[[merge_clust]])))
+  #   new_clusts
+  # }
+  
+  # }
+}
+
+# scores_to_use <- matrix(0, nrow = n_clust, ncol = n_clust)
+# scores_to_use[scores > 0.4] <- 1
+
+
+
+
+all(y1 %in% y)
+all(y1 %in% c(y, "z"))
+
+pheatmap::pheatmap(clusters[[n_clust]])
+
+length(unlist(clusters))
+length(names(unlist(clusters)))
+length(unique(names(unlist(clusters))))
+names(unlist(clusters))[duplicated(names(unlist(clusters)))]
+
+dupes <- c("3290347", "5720100", "5900441")
+
+c1_names <- names(clusters[[1]])
+c2_names <- names(clusters[[2]])
+
+ind_wanted <- row.names(psm_1) %in% c(c1_names, c2_names)
+small_psm_2 <- psm_1[ind_wanted, ind_wanted]
+pheatmap::pheatmap(small_psm_2)
+
+dupe_level <- c()
+for(i in 1:n_clust){
+  if(any(dupes %in% names(clusters[[i]]))){
+    dupe_level <- c(dupe_level, i)
+  }
+}
+clusters[[1]][[dupes[1]]]
+clusters[[4]][[dupes[1]]]
+clusters[[6]][[dupes[1]]]
+
+row.names(psm_1)[1:6]
+str(clusters)
+clusters[[6]]
+head(clusters[[1]][1:6])
+head(clusters[[2]][1:6])
+
+psm_1[1,]
+
+
+
+# Declare empty variable to capture information
+n_genes <- NA
+mdi_allocation <- list()
+allocation_list <- list()
+
+# MDI call specific values
+num_datasets <- 9
+n_iter <- 1e4
+thin <- 25
+burn <- 0.1 * (n_iter / thin)
+eff_n_iter <- n_iter / thin - burn
+
+# Create a dataframe to hold the output
+col_names <- paste0("D", 1:num_datasets)
+
+# The number of genes is the number of columns in the mcmc_output
+# exclusing the columns containing information on the phi and mass parameters
+if (is.na(n_genes)) {
+  n_genes <- mcmc_output %>%
+    select(contains("Dataset")) %>%
+    ncol() / num_datasets
+}
+
+# Create an empty dataframe with column names corresponding to dataset numbers
+compare_df <- data.frame(matrix(ncol = num_datasets, nrow = n_genes))
+# colnames(compare_df) <- col_names
+
+# The actual names of the datasets are
+files_present <- list.files(path = ".") %>%
+  grep(".csv", ., value = TRUE)
+
+# Remove the output file from this list if you saved it in the same location as
+# the data files
+if (mdi_output_file %in% files_present) {
+  index_to_empty <- files_present == mdi_output_file
+  files_present %<>%
+    list.remove(index_to_empty)
+}
+# Remove the file extension
+dataset_names <- tools::file_path_sans_ext(files_present)
+
+# Have to have the order from the call
+# CD4, CD8, CVD19, CD14, CD15, platelets, ileonic, colonic and rectal biopsies
+# transcriptome data for six circulating immune cell types (CD4+ T lymphocytes,
+# CD8+ T lymphocytes, CD19+ B lymphocytes, CD14+ monocytes, CD15+ granulocytes,
+# platelets) as well as ileal, colonic, and rectal biopsies (IL, TR, RE)
+# 323 healthy Europeans
+# files_present <- c("CD14.csv", "CD15.csv", "IL.csv", "CD4.csv", "CD8.csv", "CD19.csv", "PLA.csv", "RE.csv", "TR.csv")
+dataset_names <- tools::file_path_sans_ext(files_present)
+
+# Set the cell type to the column names (make sure order is as per MDI call)
+colnames(compare_df) <- dataset_names
+
+# === MDI output ===============================================================
+
+mdi_pos_sim_mat <- list()
+check_median_makes_sense_map <- list()
+
+# Capture the allocation information in the named lists and the predicted
+# allocation in the dataframe
+for (i in 1:num_datasets) {
+  dataset_name <- paste0("D", i)
+  
+  # Get the allocation and drop the burn in
+  mdi_allocation[[i]] <- .mdi_alloc <- getMdiAllocations(mcmc_output, i) %>%
+    .[-(1:burn), ]
+  
+  # sams_pos_sim <- genPosteriourSimilarityMatrix(.mdi_alloc)
+  # 
+  # # Individuals are columns rather than rows (henc transpose)
+  # mdi_pos_sim_mat[[i]] <- .sim_mat <- similarity_mat(t(.mdi_alloc))
+  # break
+  # By checking the imilarity of each row we can decide if the median is an 
+  # accurate method to allocate class (if all rows are highly similar label 
+  # flipping did not occur) 
+  # check_median_makes_sense_map[[i]] <- .sense_check <- similarity_mat(.mdi_alloc)
+# }
+
+# pheatmap(mdi_pos_sim_mat[[1]])
+
+  allocation_list[[i]] <- .pred_alloc <- apply(.mdi_alloc, 2, median)
+
+  compare_df[[dataset_names[i]]] <- .pred_alloc
+
+  if (i == 1) {
+    row.names(compare_df) <- names(.pred_alloc)
+  }
+}
+
+# Possibly need to rearrange based on call order
+# compare_df <- compare_df[, c(4,5,1,2,6,7,3,8,9)]
+
+# Check the number and vlaues of clusters present
+clusters_present <- apply(compare_df, 2, unique)
+
+# Set the row names
+row.names(probes_present_dt) <- probes_present_dt$V1
+
+# Remove the ID column
+probes_present_dt_no_V1 <- probes_present_dt[, -1]
+
+# Create a new dataframe with 0's for the probes not present in that dataframe
+# and the allocation label otherwise
+compare_df_2 <- as.matrix(compare_df) * as.matrix(probes_present_dt_no_V1)
+
+# compare_df_2[compare_df_2 == 0] <- NA
+
+# Add the row and column names
+row.names(compare_df_2) <- row.names(compare_df)
+colnames(compare_df_2) <- colnames(compare_df)
+
+# Check if this looks correct
+head(compare_df_2)
+
+# Move to a new labelling system (as MDI overfits the number of clusters we have
+# an unevenly distributed range of values - in our new system there is equal
+# distance between each label)
+# sort(unique(c(as.matrix(compare_df))))
+
+n_clust <- length(unique(c(compare_df_2)))
+old_labels <- sort(unique(c(compare_df_2)))
+new_labels <- 1:n_clust
+new_labels[1] <- -1
+key <- data.frame(old = old_labels, new = new_labels)
+
+# Create a dataframe with the new labels
+compare_df_new_labels <- compare_df_2
+compare_df_old_labels <- compare_df
+
+# Use [] to ensure that the structure is preserved
+compare_df_new_labels[] <- key$new[match(unlist(compare_df_2), key$old)]
+compare_df_old_labels[] <- key$new[match(unlist(compare_df), key$old)]
+
+# col_pal <- sort(brewer.pal(15, "Blues"), T)
+
+col_pal_old <- c("#DDDDDD", rev(brewer.pal(n = 11, "RdYlBu"))) # name = "RdYlBu")))
+col_pal <- c(rev(brewer.pal(n = 11, "RdYlBu"))) # name = "RdYlBu")))
+
+
+ph_full <- pheatmap(compare_df_new_labels,
+                    cluster_rows = T,
+                    cluster_cols = F,
+                    color = col_pal_old
+)
+
+
+row_order <- ph_full_old$tree_row[["order"]]
+
+
+ph_full_old <- pheatmap(compare_df_old_labels[row_order, ],
+                        cluster_rows = F,
+                        cluster_cols = F,
+                        color = col_pal
+)
+
+# Try and find an acceptable colour palette for the heatmap
+
+# col_pal <- rainbow(15, s = 0.6, v = 0.75)
+# library(fields) # install.packages("fields")
+# col_pal <- tim.colors(15)
+# library(gplots) # install.packages("gplots")
+# col_pal <- rich.colors(15)
+
+# === Heatmapping ==============================================================
+
+col_pal <- col_pal_old
+
+# Pull out the Gene IDs in the correct order
+gene_id <- probe_key %>%
+  .[match(.$ProbeID, row.names(compare_df_new_labels))] %>%
+  .$Gene
+
+# Move from Probe ID to Gene ID
+row.names(compare_df_new_labels) <- gene_id
+
+# Ceck out some specific gene sets
+psmd_ind <- grep("PSMD", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[psmd_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+PTP4_ind <- grep("PTP4", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[PTP4_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+PTPN_ind <- grep("PTPN", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[PTPN_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+CFAP_ind <- grep("CFAP", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[CFAP_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+NOD_ind <- grep("NOD", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[NOD_ind, ],
+         luster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+ATG_ind <- grep("ATG", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[ATG_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+IL_ind <- grep("^IL[1, 2]", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[IL_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+MOX_ind <- grep("MOX", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[MOX_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+HOX_ind <- grep("HOX", row.names(compare_df_new_labels))
+pheatmap(compare_df_new_labels[HOX_ind, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+
+# Heatmap of allocaiton
+ph_full <- pheatmap(compare_df_new_labels,
+                    cluster_cols = F,
+                    color = col_pal
+)
+row_order <- ph_full$tree_row[["order"]]
+
+df_ph_order <- compare_df_new_labels[row_order, ]
+
+# Inspect this in more manageable section, keeping the same order
+# contains(row.names(df_ph_order))
+pheatmap(df_ph_order[1:2500, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+pheatmap(df_ph_order[2501:5000, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+# There's very little information here
+pheatmap(df_ph_order[5001:10000, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+pheatmap(df_ph_order[10001:15000, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+# Here it quite similar
+pheatmap(df_ph_order[15001:18517, ],
+         cluster_rows = F,
+         cluster_cols = F,
+         color = col_pal
+)
+
+
+pheatmap(compare_df_2, cluster_cols = F)
+
+(n_clusters_present <- lapply(clusters_present, length))
+
+ph_full <- pheatmap(compare_df, cluster_rows = T, cluster_cols = F)
+row_order <- ph_full$tree_row[["order"]]
+col_order <- ph_full$tree_col[["order"]]
+
+df_ph_order <- compare_df[row_order, ]
+pheatmap(df_ph_order[1:2500, ], cluster_rows = F, cluster_cols = F)
+pheatmap(df_ph_order[2501:5000, ], cluster_rows = F, cluster_cols = F)
+pheatmap(df_ph_order[5001:10000, ], cluster_rows = F, cluster_cols = F)
+pheatmap(df_ph_order[10001:15000, ], cluster_rows = F, cluster_cols = F)
+pheatmap(df_ph_order[15001:18517, ], cluster_rows = F, cluster_cols = F)
+
+# comparison_sets_1 <- comparison_sets_2 <- dataset_names
+#
+# compare_df_2 <- compare_df
+#
+# for (i in 1:num_datasets) {
+#   comparison_sets_1 <- comparison_sets_1[-(comparison_sets_1 == dataset_names[i])]
+#   comparison_sets_2 <- comparison_sets_2[-(comparison_sets_2 == dataset_names[i])]
+#   curr_ds <- dataset_names[[i]]
+#   var_1 <- rlang::sym(curr_ds)
+#   for (comp_ds in comparison_sets_1) {
+#     comparison_sets_2 <- comparison_sets_2[-(comparison_sets_2 == comp_ds)]
+#     var_2 <- rlang::sym(comp_ds)
+#     for (comp_ds_2 in comparison_sets_2) {
+#       var_3 <- rlang::sym(comp_ds_2)
+#       new_var <- paste0("Common_", curr_ds, "_", comp_ds, "_", comp_ds_2) %>%
+#         rlang::sym()
+#       compare_df_2 %<>%
+#         mutate(!!new_var := !!var_1 == !!var_2 & !!var_1 == !!var_3)
+#     }
+#   }
+# }
+#
+# colnames(compare_df_2)
+# row.names(compare_df_2) <- row.names(compare_df)
+# head(compare_df_2)
+# compare_df_3 <- compare_df_2[, !(names(compare_df_2) %in% dataset_names)]
+# compare_df_3$All <- apply(compare_df_3, 1, sum)
+#
+# common_genes <- row.names(compare_df_3)[compare_df_3$All >= 1]
+#
+#
+# # compare_df_2 <- compare_df %>%
+# #   mutate(
+# #     Common_d1d2 = D1 == D2,
+# #     Common_d1d3 = D1 == D3,
+# #     Common_d2d3 = D2 == D3,
+# #     Common_d1d2d3 = D1 == D2 & D1 == D3
+# #   )
+#
+# com_genes <- compare_df[compare_df_2$Common_d1d2d3 == T, ]
+# pheatmap(com_genes)
+# row.names(com_genes)
+#
+# com_all <- sum(compare_df$Common_d1d2d3)
+# com_d1d2 <- sum(compare_df$Common_d1d2)
+# com_d1d3 <- sum(compare_df$Common_d1d3)
+# com_d2d3 <- sum(compare_df$Common_d2d3)
+#
+# sum_df <- data.frame(
+#   All = com_all,
+#   D1D2 = com_d1d2,
+#   D1D3 = com_d1d3,
+#   D2D3 = com_d2d3
+# )
+#
+# # psm_1 <- genPosteriourSimilarityMatrix(mdi_1)
+# #
+# # # pheatmap(psm_1)
+# #
+# # consensus_psm_1 <- generateConsensusPSM(mcmc_output)
--- Analysis/analysis_run_2.R
+++ Analysis/analysis_run_2.R
@@ -28,13 +28,14 @@ library(stringr)
 # output <- loadDataGauss("~/Desktop/First attempt/output_1.csv")
 
 # Function to plot a heatmap of some subset of genes
-gene_subset <- function(data, gene_str, ..., cols_to_use = 1:ncol(data)){
+gene_subset <- function(data, gene_str, ..., cols_to_use = 1:ncol(data)) {
   # Find the genes of interest within the row names of the data
   genes_of_interest <- grep(gene_str, row.names(data))
-  
+
   # Plot the heatmap
-  ph <- pheatmap(compare_mat[genes_of_interest, cols_to_use],
-                 ...
+  ph <- pheatmap(
+    compare_mat[genes_of_interest, cols_to_use],
+    ...
   )
 }
 
@@ -54,13 +55,14 @@ col_pal <- c("#DDDDDD", rev(brewer.pal(n = 11, "RdYlBu")))
 # === Gene Sets ================================================================
 
 # Function to plot a heatmap of some subset of genes
-gene_subset <- function(data, gene_str, ..., cols_to_use = 1:ncol(data)){
+gene_subset <- function(data, gene_str, ..., cols_to_use = 1:ncol(data)) {
   # Find the genes of interest within the row names of the data
   genes_of_interest <- grep(gene_str, row.names(data))
-  
+
   # Plot the heatmap
-  ph <- pheatmap(compare_mat[genes_of_interest, cols_to_use],
-                 ...
+  ph <- pheatmap(
+    compare_mat[genes_of_interest, cols_to_use],
+    ...
   )
 }
 
@@ -70,90 +72,94 @@ cd_subset <- c(1, 3, 4, 5)
 intestine_subset <- c(6, 8, 9)
 
 # Search for the HOX genes
-gene_subset(compare_mat, "HOX", cluster_rows = T,
-            cluster_cols = T,
-            color = col_pal,
-            main = "HOX genes in CD datasets",
-            cols_to_use = cd_subset)
+gene_subset(compare_mat, "HOX",
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal,
+  main = "HOX genes in CD datasets",
+  cols_to_use = cd_subset
+)
 
-gene_subset(compare_mat, "HOX", cluster_rows = T,
-            cluster_cols = T,
-            color = col_pal,
-            main = "HOX genes in colonic datasets",
-            cols_to_use = intestine_subset)
+gene_subset(compare_mat, "HOX",
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal,
+  main = "HOX genes in colonic datasets",
+  cols_to_use = intestine_subset
+)
 
 
 
 # Ceck out some specific gene sets
 psmd_ind <- grep("PSMD", row.names(compare_mat))
 pheatmap(compare_mat[psmd_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 PTP4_ind <- grep("PTP4", row.names(compare_mat))
 pheatmap(compare_mat[PTP4_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 PTPN_ind <- grep("PTPN", row.names(compare_mat))
 pheatmap(compare_mat[PTPN_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 CFAP_ind <- grep("CFAP", row.names(compare_mat))
 pheatmap(compare_mat[CFAP_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 NOD_ind <- grep("NOD", row.names(compare_mat))
 pheatmap(compare_mat[NOD_ind, ],
-         luster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  luster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 ATG_ind <- grep("ATG", row.names(compare_mat))
 pheatmap(compare_mat[ATG_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 IL_ind <- grep("^IL[1, 2]", row.names(compare_mat))
 pheatmap(compare_mat[IL_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 MOX_ind <- grep("MOX", row.names(compare_mat))
 pheatmap(compare_mat[MOX_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 HOX_ind <- grep("HOX", row.names(compare_mat))
 pheatmap(compare_mat[HOX_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 
 LBP_ind <- grep("LBP", row.names(compare_mat))
 pheatmap(compare_mat[LBP_ind, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 
@@ -163,9 +169,9 @@ CD3_ind <- grep("^CD3[A-Z]", row.names(compare_mat))
 CD4_ind <- grep("CD4$", row.names(compare_mat))
 
 pheatmap(compare_mat[c(TCR_ind, CD3_ind, CD4_ind), 1:5],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 
@@ -174,37 +180,37 @@ pheatmap(compare_mat[c(TCR_ind, CD3_ind, CD4_ind), 1:5],
 # CD31 | PTK7 | CR | IL8
 CD_31_ind <- grep("CD31", row.names(compare_mat))
 pheatmap(compare_mat[CD_31_ind, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 PTK_ind <- grep("PTK", row.names(compare_mat))
 pheatmap(compare_mat[PTK_ind, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 CR_ind <- grep("^CR[1, 2]", row.names(compare_mat))
 pheatmap(compare_mat[CR_ind, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 IL_ind <- grep("^IL[5-9]", row.names(compare_mat))
 pheatmap(compare_mat[IL_ind, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 
-# Causal genes 
+# Causal genes
 
 # In the case of IBD, causative genes have been identified for approximately ten
-# risk loci on the basis of such “independently” (i.e., not merely reflecting LD 
+# risk loci on the basis of such “independently” (i.e., not merely reflecting LD
 # with other variants) associated coding variants, including:
 # NOD2, ATG16L1, IL23R, CARD9, FUT2, and TYK2
 # Source: IBD risk loci are enriched in multigenic regulatory modules encompassing putative causative genes
@@ -218,61 +224,65 @@ CARD_ind <- grep("CARD9", row.names(compare_mat))
 
 IBD_genes <- c(NOD_ind, ATG_ind, IL23_ind, FUT_ind, TYK_ind, CARD_ind)
 pheatmap(compare_mat[IBD_genes, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 CD_ind <- grep("^CD[1, 4, 8]", row.names(compare_mat))
 pheatmap(compare_mat[CD_ind, ],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 
 HLA_ind <- grep("HLA", row.names(compare_mat))
-pheatmap(compare_mat[HLA_ind,],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+pheatmap(compare_mat[HLA_ind, ],
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 IL12_ind <- grep("IL12", row.names(compare_mat))
-pheatmap(compare_mat[IL12_ind,],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+pheatmap(compare_mat[IL12_ind, ],
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 CCL_ind <- grep("CCL", row.names(compare_mat))
-pheatmap(compare_mat[CCL_ind,],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+pheatmap(compare_mat[CCL_ind, ],
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 TNF_ind <- grep("TNF", row.names(compare_mat))
 pheatmap(compare_mat[TNF_ind, c(1, 3, 4, 5)],
-         cluster_rows = T,
-         cluster_cols = T,
-         color = col_pal
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal
 )
 
 
 
 
-gene_subset(compare_mat, "TNF", cluster_rows = T,
-            cluster_cols = T,
-            color = col_pal,
-            main = "TNF genes in CD datasets",
-            cols_to_use = cd_subset)
+gene_subset(compare_mat, "TNF",
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal,
+  main = "TNF genes in CD datasets",
+  cols_to_use = cd_subset
+)
 
-gene_subset(compare_mat, "TNF", cluster_rows = T,
-            cluster_cols = T,
-            color = col_pal,
-            main = "TNF genes in colonic datasets",
-            cols_to_use = intestine_subset)
+gene_subset(compare_mat, "TNF",
+  cluster_rows = T,
+  cluster_cols = T,
+  color = col_pal,
+  main = "TNF genes in colonic datasets",
+  cols_to_use = intestine_subset
+)
 
 
 
@@ -285,8 +295,8 @@ gene_subset(compare_mat, "CCL", intestine_subset)
 
 # Heatmap of allocaiton
 ph_full <- pheatmap(compare_mat,
-                    cluster_cols = F,
-                    color = col_pal
+  cluster_cols = F,
+  color = col_pal
 )
 row_order <- ph_full$tree_row[["order"]]
 
@@ -295,35 +305,35 @@ df_ph_order <- compare_mat[row_order, ]
 # Inspect this in more manageable section, keeping the same order
 # contains(row.names(df_ph_order))
 pheatmap(df_ph_order[1:2500, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 pheatmap(df_ph_order[2501:5000, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 # There's very little information here
 pheatmap(df_ph_order[5001:10000, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 pheatmap(df_ph_order[10001:15000, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 # Here it quite similar
 pheatmap(df_ph_order[15001:18517, ],
-         cluster_rows = F,
-         cluster_cols = F,
-         color = col_pal
+  cluster_rows = F,
+  cluster_cols = F,
+  color = col_pal
 )
 
 
@@ -333,48 +343,48 @@ n_clusters_present <- lapply(clusters_present, length)
 
 # === Similar datasets =========================================================
 pheatmap::pheatmap(compare_mat[, c(1, 3, 4, 5)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for CD datasets (all genes)"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for CD datasets (all genes)"
 )
 
 pheatmap::pheatmap(compare_mat[, c(6, 8, 9)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for intestinal datasets (all genes)"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for intestinal datasets (all genes)"
 )
 
-# Less similar 
+# Less similar
 
 pheatmap::pheatmap(compare_mat[, c(1, 3, 4, 5, 6, 8, 9)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for intestinal & CD datasets (all genes)"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for intestinal & CD datasets (all genes)"
 )
 
 
 pheatmap::pheatmap(compare_mat[, c(7, 2, 1, 3, 4, 5)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for CD datasets + dissimilar datasets"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for CD datasets + dissimilar datasets"
 )
 
 pheatmap::pheatmap(compare_mat[, c(2, 7, 6, 8, 9)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for intestinal datasets + dissimilar datasets"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for intestinal datasets + dissimilar datasets"
 )
 
 # Pairwise
 colnames(compare_mat)
 pheatmap::pheatmap(compare_mat[, c(1, 4)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for CD14 and CD4 (all genes)"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for CD14 and CD4 (all genes)"
 )
 
 pheatmap::pheatmap(compare_mat[, c(3, 4)],
-                   cluster_cols = T,
-                   color = col_pal,
-                   main = "Heatmap for CD19 and CD4 (all genes)"
+  cluster_cols = T,
+  color = col_pal,
+  main = "Heatmap for CD19 and CD4 (all genes)"
 )
