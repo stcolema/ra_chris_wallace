4013b5c107bd015280a41861bd85b2c0834b1433
Stephen Coleman <stcolema@tcd.ie>
2019-05-15 19:42:43 +0100

    Script to install the various packages used by the scripts if not present.

---
+++ install_packages.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+# Script to install the required packages if not already installed
+
+required_packages <- c(
+  "tidyverse",
+  "mcclust",
+  "mclust",
+  "pheatmap",
+  "Rcpp",
+  "RcppArmadillo",
+  "lpSolve",
+  "data.table",
+  "optparse",
+  "magrittr",
+  "RColorBrewer"
+)
+
+for (package in required_packages) {
+  if (!(package %in% rownames(installed.packages()))) {
+    install.packages(package, dep = T)
+  }
+}
