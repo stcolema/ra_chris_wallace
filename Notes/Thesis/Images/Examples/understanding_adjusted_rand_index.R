7e0e65d7a5bcfa061339e06150efc688faef00ad
Stephen Coleman <stcolema@tcd.ie>
2019-06-25 11:25:58 +0100

    More on the rand index, some re-arranging of the order.

--- Notes/Thesis/Images/Examples/understanding_adjusted_rand_index.R
+++ Notes/Thesis/Images/Examples/understanding_adjusted_rand_index.R
@@ -2,24 +2,35 @@
 
 set.seed(1)
 K <- 3
-n <- 1e3
-x <- c(rep(1, 3874), sample(1:K, size = n, replace = T))
-y <- c(rep(1, 3874), sample(1:K, size = n, replace = T))
+n <- 1e4
 
+n_1 <- (7/16) * n
+n_2 <- (9/16) * n
 
-
-# comb_data <- data.frame(X = x, Y = y)
-# 
-# cont_table <- matrix(0, nrow = K, ncol = K)
-# for(i in 1:K){
-#   for(j in 1:K){
-#     cont_table[i, j] <- sum(x == i & y == j) / n
-#   }
-# }
-# 
-# cont_table
-(cont_table_2 <- table(x, y) / n) 
+x <- c(rep(1, n_1), sample(1:K, size = n_2, replace = T))
+y <- c(rep(1, n_1), sample(1:K, size = n_2, replace = T))
+(cont_table_2 <- table(x, y) / n ) 
 
 mcclust::arandi(x , y)
 fossil::rand.index(x, y)
-fossil:::adj.rand.index(x, y)
+
+x_2 <- sample(1:K, size = n, replace = T)
+y_2 <- sample(1:K, size = n, replace = T)
+(cont_table_2 <- table(x_2, y_2) / n ) 
+
+mcclust::arandi(x_2 , y_2)
+fossil::rand.index(x_2, y_2)
+
+# Data_x_y <- data.frame(X = x_2, Y = y_2)
+# 
+# for(i in 1:K){
+# Data_x_y$X[Data_x_y$X == i & Data_x_y$Y == i] <- (i %% K) + 1
+# Data_x_y$X[Data_x_y$X == i & Data_x_y$Y == i] <- (i %% K) + 2
+# }
+# 
+# (cont_table_2 <- table(Data_x_y$X, Data_x_y$Y) / n ) 
+# 
+# 
+# mcclust::arandi(Data_x_y$X , Data_x_y$X)
+# fossil::rand.index(Data_x_y$X, Data_x_y$X)
+
