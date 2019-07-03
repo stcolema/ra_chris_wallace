9d14fcc276523a83c2a21f3efb4e0be07852d330
Stephen Coleman <stcolema@tcd.ie>
2019-07-03 19:27:29 +0100

    Plots! For convergence and comparisons.

--- Gen_data/compare_true_clustering.R
+++ Gen_data/compare_true_clustering.R
@@ -28,37 +28,25 @@ for (f in paste0(function_dir, function_scripts)) {
   source(f)
 }
 
-gen_dir <- "/home/MINTS/sdc56/Desktop/Gen_data_output"
+# For controlling ordering of boxplots
+add_level <- function(x, new_level){
+  if(is.factor(x)) return(factor(x, levels=c(levels(x), new_level)))
+  return(x)
+}
 
-all_dirs_in_full <- list.dirs(path = gen_dir, recursive = F)
+# === Main script ==============================================================
 
-longer_dirs <- all_dirs_in_full %>% 
-  extract(grep("Longer" , .))
+# Set ggplot2 theme
+theme_set(theme_bw() + theme(axis.text.x = element_text(angle = 30, hjust=1)))
 
-consensus_dirs <- all_dirs_in_full %>% 
-  extract(grep("Many" , .)) 
+# The directroy holding the data
+gen_dir <- "/home/MINTS/sdc56/Desktop/Gen_data_output/MDI_output"
 
-# gen_long_run_name <- c("Long", "Longer")
-# gen_many_seeds_name <- "Many_seeds_"
+# Directroy to save plots to
+plot_dir <- "Notes/Thesis/Images/Gen_data/Case_2"
+dir.create(plot_dir, showWarnings = F)
 
-
-all_rel_dirs <- c(longer_dirs, consensus_dirs)
-n_long <- length(longer_dirs)
-long_types <- rep("Long", n_long)
-long_types <- paste("Long", 1:n_long)
-consensus_types <- paste("Consensus", c(1000, 10000, 500, 5000))
-types <- c(long_types, consensus_types)
-
-tib_name <- "compare_tibble.rds"
-all_data <- paste0(all_rel_dirs, "/", tib_name)
-
-all_tibs <- lapply(all_data, readRDS)
-
-# === Set up to record =========================================================
-
-# my_tib <- readRDS("/home/MINTS/sdc56/Desktop/Yeast_MDI/Yeast_output/Many_seeds_1000_iter/compare_tibble.rds")
-# weights <- c(0.0, 0.2, 0.4, 0.6, 0.8, 1, 2, 3, 4, 5)
-
+# The true labelling for this generated data
 true_labels <- c(rep(1, 25),
                  rep(2, 50),
                  rep(3, 75),
@@ -66,32 +54,74 @@ true_labels <- c(rep(1, 25),
                  rep(5, 150)
 )
 
+# All directories holding MDI output tibbles
+all_dirs_in_full <- list.dirs(path = gen_dir, recursive = F)
+all_dirs_short <- list.dirs(path = gen_dir, recursive = F, full.names = F)
+
+# Select the Long chain dirs
+longer_dirs <- all_dirs_in_full %>% 
+  extract(grep("Long" , .))
+
+# select the consensus dirs
+consensus_dirs <- all_dirs_in_full %>% 
+  extract(grep("Consensus" , .)) 
+
+tib_types <- list.dirs(path = gen_dir, recursive = F, full.names = F)
+
+# gen_long_run_name <- c("Long", "Longer")
+# gen_many_seeds_name <- "Many_seeds_"
+
+# This is now redundant but goes back to bad folder organisation
+all_rel_dirs <- all_dirs_in_full # c(consensus_dirs, longer_dirs)
+
+# I don't think this is used now
+types <- stringr::str_replace(tib_types, "_", " ")
+
+# The name of the object the MDI output data is saved to in each dir in all_dirs_in_full
+tib_name <- "compare_tibble.rds"
+all_data <- paste0(all_rel_dirs, "/", tib_name)
+
+# Read in the tibbles
+all_tibs <- lapply(all_data, readRDS)
+
+# Separate out the types of MDI applied
+consensus_types <- tib_types %>% extract(grep("Consensus", .))
+long_types <- tib_types %>% extract(grep("Long", .))
+
+# Used in making nicer x-axis annotation
+nice_types <- c(str_replace(consensus_types, "_", " "), str_replace(long_types, "_", " run: seed " ))
+
+
+# === Set up to record =========================================================
+
+
+# The object that will hold the comparison data
 comparison_data <- tibble(
-  # Clustering = list(),
   Adjusted_rand_index = numeric(),
   Dataset = character(),
   Type = character()
-  # Seed = numeric()
 )
 
+# The object that will hold the current iteration of the loop
 comparison_data_entry <- comparison_data
 
-# 1000_weight_1
-
+# Datasets present
 datasets <- all_tibs[[1]]$dataset
 n_datasets <- length(datasets)
 
+# Number of MDI outputs here
 n_mdi <- length(all_data)
+
+# Loop over different MDI outputs and save comparison to true allocation
 for(i in 1:n_mdi){
   curr_tib <- all_tibs[[i]]
   curr_type <- types[i]
+  curr_nice_type <- nice_types[i]
 for(d in datasets){
   curr_index <- which(curr_tib$dataset == d)
   
   curr_clustering <- curr_tib$mdi_allocation[[curr_index]]
   
-  # curr_clustering <- curr_tib$pred_allocation[[curr_index]] %>% unname()
-  # curr_adj_rand <- mcclust::arandi(true_labels, curr_clustering)
   
   curr_rand_vec <- apply(curr_clustering, 1, unlist_arandi, true_labels)
   
@@ -99,129 +129,67 @@ for(d in datasets){
   comparison_data_entry <- tibble(
     Adjusted_rand_index = curr_rand_vec,
     Dataset = d,
-    Type = curr_type
+    Type = curr_nice_type
   )
-  
-  # cat("\n\nDataset: ", d)
-  # cat("\nType: ", curr_type)
-  # cat("\nRand index: ", curr_adj_rand)
-  # cat("\nCurr clustering (head)\n: ", head(curr_clustering))
-  # cat("\n\nCurr clustering (str)\n: ", str(curr_clustering))
   
   comparison_data <- rbind(comparison_data, comparison_data_entry)
 }
 }
 
-# small_subset <- c(11:14)
-# small_tibs <- all_tibs[small_subset]
-# small_types <- types[small_subset]
-# small_comparison <-  tibble(
-#   Adjusted_rand_index = numeric(),
-#   Dataset = character(),
-#   Type = character()
-# )
-# 
-# n_mdi_small <- length(small_types)
-# for(i in 1:n_mdi_small){
-#   curr_tib <- small_tibs[[i]]
-#   curr_type <- small_types[i]
-#   for(d in datasets){
-#     curr_index <- which(curr_tib$dataset == d)
-#     curr_clustering <- curr_tib$mdi_allocation[[curr_index]]
-#     curr_rand_vec <- apply(curr_clustering, 1, unlist_arandi, true_labels)
-# 
-#     # curr_adj_rand <- mcclust::arandi(true_labels, curr_clustering)
-#     comparison_data_entry <- tibble(
-#       Adjusted_rand_index = curr_rand_vec,
-#       Dataset = d,
-#       Type = curr_type
-#     )
-#     
-#     small_comparison <- rbind(small_comparison, comparison_data_entry)
-#   }
-# }
+# === Plotting =============================================================
 
-# mdi_alloc <- all_tibs[[1]]$mdi_allocation[[1]][1:10, 1:10]
-# 
-# 
-# rand[[i + (j - 1) * n_files]] <- apply(
-#   mdi_allocation_lst[i + (j - 1) * n_files][[1]],
-#   # mdi_allocation[[i]],
-#   1,
-#   # mcclust::arandi,
-#   unlist_arandi,
-#   # mclust::adjustedRandIndex,
-#   mdi_allocation_lst[i][[1]][eff_n_iter - start_index, ]
-#   # compare_df[, i]
-# )
-# 
-# 
-# consesnsus_tibble <- comparison_data[comparison_data$Type %in% consensus_types, ]
+# Find the appropriate order for boxplots
+new_order <- unique(comparison_data$Type)[c(3, 1, 4, 2, 5, 7:14, 6)]
+comparison_data_1 <- within(comparison_data, 
+                          Type <- factor(Type, 
+                                          levels=new_order)
+)
 
-ggplot(data = comparison_data, aes(x = Type, y = Adjusted_rand_index)) +
-  geom_violin() +
+
+bp <- ggplot(data = comparison_data_1, aes(x = Type, y = Adjusted_rand_index)) +
+  geom_boxplot() +
   facet_wrap(~Dataset) +
-  theme(axis.text.x = element_text(angle = 30, hjust=1)) +
+  # theme(axis.text.x = element_text(angle = 30, hjust=1)) +
   labs(
-    title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
+    title = "Generated data: comparison of consensus clusterings and individual chains",
     y = "Adjusted rand index"
     
   )
-  # facet_wrap_paginate(~ Dataset + Weight)
+
+
+ggsave(paste0(plot_dir, "/box_plot_ari_true_clustering.png"), plot = bp)
 
 comparison_data_2 <- comparison_data
+comparison_data_2$Type[comparison_data_2$Type %in% long_nice_types] <- "Long"
+new_order_2 <- unique(comparison_data_2$Type)[c(3, 1, 4, 2, 5)]
+comparison_data_2 <- within(comparison_data_2, 
+                            Type <- factor(Type, 
+                                           levels=new_order_2)
+)
 
-comparison_data_2$Type[comparison_data_2$Type %in% long_types] <- "Long"
 
-ggplot(data = comparison_data, aes(x = Type, y = Adjusted_rand_index)) +
-  geom_boxplot(notch = T) +
+bp_collapsed <- ggplot(data = comparison_data_2, aes(x = Type, y = Adjusted_rand_index)) +
+  geom_boxplot() +
   facet_wrap(~Dataset) +
   theme(axis.text.x = element_text(angle = 30, hjust=1)) +
   labs(
-    title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
+    title = "Generated data: comparison of consensus clusterings and collapsed chains",
+    # subtitle = "Consensus clustering for various iterations, long runs corresponds to 10 chains of 2 million iterations",
     y = "Adjusted rand index"
     
   )
+ggsave(paste0(plot_dir, "/box_plot_ari_true_clustering_collapsed_long.png"), plot = bp_collapsed)
 
-ggplot(data = comparison_data, aes(x = Type, y = Adjusted_rand_index)) +
-  geom_boxplot() +
-  facet_wrap_paginate(~Dataset, nrow = 1, ncol = 1) +
-  theme(axis.text.x = element_text(angle = 30, hjust=1)) +
-  labs(
-    title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
-    y = "Adjusted rand index"
-    
-  )
 
-ggplot(data = comparison_data_2, aes(x = Type, y = Adjusted_rand_index)) +
+vp <- ggplot(data = comparison_data_2, aes(x = Type, y = Adjusted_rand_index)) +
   geom_violin() +
   facet_wrap(~Dataset) +
   theme(axis.text.x = element_text(angle = 30, hjust=1)) +
   labs(
-    title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
+    title = "Generated data: comparison of consensus clusterings and collapsed chains",
+    # subtitle = "Consensus clustering for various iterations, long runs corresponds to 10 chains of 2 million iterations",
     y = "Adjusted rand index"
     
   )
 
-
-# 
-# ggplot(data = small_comparison, aes(x = Type, y = Adjusted_rand_index)) +
-#   geom_boxplot() +
-#   facet_wrap(~Dataset) +
-#   theme(axis.text.x = element_text(angle = 30, hjust=1)) +
-#   labs(
-#     title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
-#     y = "Adjusted rand index"
-#     
-#   )
-# 
-# 
-# ggplot(data = consesnsus_tibble, aes(x = Type, y = Adjusted_rand_index)) +
-#   geom_point() +
-#   facet_wrap(~Dataset) +
-#   theme(axis.text.x = element_text(angle = 30, hjust=1)) +
-#   labs(
-#     title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
-#     y = "Adjusted rand index"
-#     
-#   )
+ggsave(paste0(plot_dir, "/violin_plot_ari_true_clustering_collapsed_long.png"), plot = vp)
--- Yeast/yeast_3_datasets_true_allocation.R
+++ Yeast/yeast_3_datasets_true_allocation.R
@@ -1,12 +1,49 @@
 
-library(tibble)
+
 library(mcclust)
 library(magrittr)
 library(ggplot2)
 library(ggforce)
+
+
+# For tibbles
+library(tibble) # for dataframe of lists
+
+# For data wrangling
+library(dplyr)
+
+# Heatmapping
+library(pheatmap) # install.packages("pheatmap", dep = T)
+
+# Colour palettes
+library(RColorBrewer)
+
+# To load true clustering
 library(rlist)
 
-true_clusters_2 <- readRDS("~/Desktop/ra_chris_wallace/Yeast/true_clusters_for_yeast_data.rds")
+
+function_dir <- "/home/MINTS/sdc56/Desktop/ra_chris_wallace/Analysis/Analysis_script_functions/"
+
+function_scripts <- c(
+  "plot_rand_index.R"
+)
+
+for (f in paste0(function_dir, function_scripts)) {
+  source(f)
+}
+
+# For controlling ordering of boxplots
+add_level <- function(x, new_level){
+  if(is.factor(x)) return(factor(x, levels=c(levels(x), new_level)))
+  return(x)
+}
+
+# === Main script ==============================================================
+
+# Set ggplot2 theme
+theme_set(theme_bw() + theme(axis.text.x = element_text(angle = 30, hjust=1)))
+
+true_clusters <- readRDS("~/Desktop/ra_chris_wallace/Yeast/true_clusters_for_yeast_data.rds")
 
 function_dir <- "/home/MINTS/sdc56/Desktop/ra_chris_wallace/Analysis/Analysis_script_functions/"
 
@@ -20,7 +57,7 @@ for (f in paste0(function_dir, function_scripts)) {
 
 
 comparison_data <- tibble(
-  Clustering = list(),
+  # Clustering = list(),
   Adjusted_rand_index = numeric(),
   Dataset = character(),
   Type = character() #,
@@ -30,8 +67,8 @@ comparison_data <- tibble(
 
 comparison_data_entry <- comparison_data
 
-types <- c("500", "1000", "long_run")
-nice_types <- c("500", "1000", "Long run")
+# types <- c("500", "1000", "long_run")
+# nice_types <- c("500", "1000", "Long run")
 # many_seeds_sub_types <- c("500", "1000")
 long_run_seeds <- 1:5
 
@@ -39,18 +76,53 @@ long_run_seeds <- 1:5
 
 gen_dir <- "/home/MINTS/sdc56/Desktop/Yeast_3_datasets/MDI_output"
 
-rel_dirs <- list.dirs(gen_dir, recursive = F)
-tibble_names <- paste0(rel_dirs, "/compare_tibble.rds")
-all_tibbles <- lapply(tibble_names, readRDS)
-consensus_types <- c(500, 1000, 5000, 10000)
-long_types <- 1:10
+all_dirs_in_full <- list.dirs(path = gen_dir, recursive = F)
+# all_dirs_short <- list.dirs(path = gen_dir, recursive = F, full.names = F)
 
-datasets <- all_tibbles[[1]]$dataset
-types <- c(consensus_types, long_types)
+longer_dirs <- all_dirs_in_full %>% 
+  extract(grep("Long" , .))
 
-n_types <- length(types)
+consensus_dirs <- all_dirs_in_full %>% 
+  extract(grep("Consensus" , .)) 
 
-nice_types <- c(paste0("Consensus ", consensus_types), paste0("Long run: seed ", long_types))
+tib_types <- list.dirs(path = gen_dir, recursive = F, full.names = F)
+
+# gen_long_run_name <- c("Long", "Longer")
+# gen_many_seeds_name <- "Many_seeds_"
+
+
+all_rel_dirs <- all_dirs_in_full # c(consensus_dirs, longer_dirs)
+# n_long <- length(longer_dirs)
+# long_types <- rep("Long", n_long)
+# long_types <- paste("Long", 1:n_long)
+# consensus_types <- paste("Consensus", c(1000, 10000, 500, 5000))
+consensus_types <- all_dirs_short %>% 
+  extract(grep("Consensus" , .)) 
+
+longer_dirs <- all_dirs_short %>% 
+  extract(grep("Long" , .))
+
+
+# types <- c(consensus_types, long_types)
+
+types <- stringr::str_replace(tib_types, "_", " ")
+
+# types <- types[match(stringr::str_replace(tib_types, "_", " "), types)]
+
+tib_name <- "compare_tibble.rds"
+all_data <- paste0(all_rel_dirs, "/", tib_name)
+
+all_tibs <- lapply(all_data, readRDS)
+
+types <- tib_types
+
+consensus_types <- tib_types %>% extract(grep("Consensus", .))
+long_types <- tib_types %>% extract(grep("Long", .))
+
+long_nice_types <- str_replace(long_types, "_", " run: seed ")
+nice_types <- c(str_replace(consensus_types, "_", " "), str_replace(long_types, "_", " run: seed " ))
+
+# === Recording data ===========================================================
 
 for (j in 1:n_types) {
   curr_type <- types[j]
@@ -68,7 +140,7 @@ for (j in 1:n_types) {
     
     comparison_data_entry <- tibble(
       Dataset = d,
-      Clustering = list(.curr_clusters),
+      # Clustering = list(.curr_clusters),
       Adjusted_rand_index = curr_rand_vec,
       Type = paste0(curr_nice_type) #,
       # Seed = k # ,
@@ -79,17 +151,66 @@ for (j in 1:n_types) {
   }
 }
 
-comparison_data_plot <- comparison_data[comparison_data$Dataset == "MDItestdata1", ]
+# === Plotting =================================================================
 
-ggplot(data = comparison_data, aes(x = Type, y = Adjusted_rand_index)) +
+new_order <- unique(comparison_data$Type)[c(3, 1, 4, 2, 5, 7:14, 6)]
+comparison_data_1 <- within(comparison_data, 
+                            Type <- factor(Type, 
+                                           levels=new_order)
+)
+
+
+bp <- ggplot(data = comparison_data_1, aes(x = Type, y = Adjusted_rand_index)) +
   geom_boxplot() +
-  # facet_wrap_paginate(~ Dataset, ncol = 1, nrow = 3, page = 1) +
   facet_wrap(~Dataset) +
-  theme(axis.text.x = element_text(angle = 30, hjust=1, size=10)) +
+  # theme(axis.text.x = element_text(angle = 30, hjust=1)) +
   labs(
-    title = "Comparison consensus clustering for various iterations to long run (2 million iterations)",
+    title = "Generated data: comparison of consensus clusterings and individual chains",
     y = "Adjusted rand index"
     
   )
 
-ggsave("comparison_ARI_yeast_data.png")
+plot_dir <- "Notes/Thesis/Images/Gen_data/Case_1"
+dir.create(plot_dir, showWarnings = F)
+
+ggsave(paste0(plot_dir, "/box_plot_ari_true_clustering.png"), plot = bp)
+
+
+# long_nice_types <- paste("Long run: seed", 1:10)
+
+comparison_data_2 <- comparison_data
+comparison_data_2$Type[comparison_data_2$Type %in% long_nice_types] <- "Long"
+new_order_2 <- unique(comparison_data_2$Type)[c(3, 1, 4, 2, 5)]
+comparison_data_2 <- within(comparison_data_2, 
+                            Type <- factor(Type, 
+                                           levels=new_order_2)
+)
+
+
+bp_collapsed <- ggplot(data = comparison_data_2, aes(x = Type, y = Adjusted_rand_index)) +
+  geom_boxplot() +
+  facet_wrap(~Dataset) +
+  theme(axis.text.x = element_text(angle = 30, hjust=1)) +
+  labs(
+    title = "Generated data: comparison of consensus clusterings and collapsed chains",
+    # subtitle = "Consensus clustering for various iterations, long runs corresponds to 10 chains of 2 million iterations",
+    y = "Adjusted rand index"
+    
+  )
+ggsave(paste0(plot_dir, "/box_plot_ari_true_clustering_collapsed_long.png"), plot = bp_collapsed)
+
+
+
+vp <- ggplot(data = comparison_data_2, aes(x = Type, y = Adjusted_rand_index)) +
+  geom_violin() +
+  facet_wrap(~Dataset) +
+  theme(axis.text.x = element_text(angle = 30, hjust=1)) +
+  labs(
+    title = "Generated data: comparison of consensus clusterings and collapsed chains",
+    # subtitle = "Consensus clustering for various iterations, long runs corresponds to 10 chains of 2 million iterations",
+    y = "Adjusted rand index"
+    
+  )
+
+ggsave(paste0(plot_dir, "/violin_plot_ari_true_clustering_collapsed_long.png"), plot = vp)
+
