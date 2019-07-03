22a5276fe8f0a8d5e3338815a0928946df12fbf1
Stephen Coleman <stcolema@tcd.ie>
2019-07-03 11:18:35 +0100

    Added referecne for NOD-like and IBD

--- Analysis/plot_missingness.R
+++ Analysis/plot_missingness.R
@@ -8,6 +8,11 @@ library(data.table)
 library(tidyr)
 library(reshape2)
 
+add_level <- function(x, new_level){
+  if(is.factor(x)) return(factor(x, levels=c(levels(x), new_level)))
+  return(x)
+}
+
 probes_present_file <- "/home/MINTS/sdc56/Desktop/Full_data_29_05_19/Meta_data/probes_present_per_dataset.csv"
 probe_key_file <- "/home/MINTS/sdc56/Desktop/ra_chris_wallace/Analysis/probe_key_unique_names.csv"
 
@@ -42,7 +47,7 @@ probes_present_dt$Gene_names <- gene_id[which(probes_present_dt$V1 %in% probe_na
 
 probes_present_dt <- probes_present_dt %>% 
   mutate(All = (CD14 + CD15 + CD19 + CD4 + CD8 + IL + PLA + RE + TR == 9),
-         All_not_PLA = (CD14 + CD15 + CD19 + CD4 + CD8 + IL + RE + TR == 8))
+         `All (excl. PLA)` = (CD14 + CD15 + CD19 + CD4 + CD8 + IL + RE + TR == 8))
 
 
 gathered_data <- gather(probes_present_dt, Dataset, Present, -V1, -Gene_names)
@@ -55,8 +60,31 @@ gathered_data$Dataset <- gathered_data$Dataset %>% as.factor
 # gathered_data$value <- gathered_data$value %>% as.factor
 
 summary(gathered_data)
+# gathered_data$Dataset <- add_level(gathered_data$Dataset, "All datasets excl. PLA")
+# gathered_data$Dataset[is.na(gathered_data$Dataset)] <- "All datasets excl. PLA"
+
+
+new_order <- levels(gathered_data$Dataset)[c(3:8, 10:11, 9, 1, 2)]
+
+gathered_data <- within(gathered_data, 
+                   Dataset <- factor(Dataset, 
+                                      levels=new_order)
+)
+
+sum(gathered_data$Present[gathered_data$Dataset == "PLA"])
+
+n_pres <- c()
+for(d in new_order[1:9]){
+  n_pres <- c(n_pres, sum(gathered_data$Present[gathered_data$Dataset == d]))
+}
+
+mean(n_pres)
+mean(n_pres[-9])
 
 ggplot(gathered_data, aes(Dataset, ..count..)) +
   geom_bar(aes(fill = Present), position = "dodge") +
   labs(title = "Probe presence across datasets",
-       y = "Count")
+       y = "Count") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 30, hjust=1))
+  
